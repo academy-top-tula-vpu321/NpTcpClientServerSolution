335486a2dfe4ee39a8f86d74fedc007f9a3775cd
Преподаватель <@DESKTOP-FC1SLNT>
2024-10-27 13:41:03 +0300

    Добавьте файлы проекта.

---
+++ NpTcpClientApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// TcpClient
+
+using System.Net.Sockets;
+
+using TcpClient client = new();
+
+try
+{
+    await client.ConnectAsync(System.Net.IPAddress.Loopback, 5000);
+    Console.WriteLine($"client {client.Client.LocalEndPoint} connected");
+}
+catch(Exception)
+{
+    Console.WriteLine("client not connected");
+}
---
+++ NpTcpListenerApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// TcpListener
+
+using System.Net;
+using System.Net.Sockets;
+
+TcpListener server = new TcpListener(IPAddress.Loopback, 5000);
+
+server.Start();
+Console.WriteLine("Server starting");
+
+while(true)
+{
+    TcpClient client = await server.AcceptTcpClientAsync();
+    Console.WriteLine($"Server accept client: {client.Client.RemoteEndPoint}");
+}
+
+
